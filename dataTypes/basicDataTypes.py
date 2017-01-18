5cea37ceb5f56e7a5a1216996b11bee66b5351c7
Aralya Phinith <aphinith84@gmail.com>
2017-01-17 23:23:55 -0800

    creating list and using append method

--- dataTypes/basicDataTypes.py
+++ dataTypes/basicDataTypes.py
@@ -1,4 +1,10 @@
 alpha = 'abcdefghi'
-print('This is alpha[3:]: ', alpha[3:])
-print('this is alpha[:4]: ', alpha[:4])
+# print('This is alpha[3:]: ', alpha[3:])
+# print('this is alpha[:4]: ', alpha[:4])
+# print('only get def: ', alpha[3:6])
 
+my_list = ['a', 'b', 'c', 'd']
+print('Before append: ', my_list)
+
+my_list.append('e')
+print('After append: ', my_list)
