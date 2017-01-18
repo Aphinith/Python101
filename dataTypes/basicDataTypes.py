758adc783cc0b31bb4b404c9b42afb2d72213dc7
Aralya Phinith <aphinith84@gmail.com>
2017-01-17 23:33:35 -0800

    creating tuples

--- dataTypes/basicDataTypes.py
+++ dataTypes/basicDataTypes.py
@@ -4,7 +4,17 @@ alpha = 'abcdefghi'
 # print('only get def: ', alpha[3:6])
 
 my_list = ['a', 'b', 'c', 'd']
-print('Before append: ', my_list)
+# print('Before append: ', my_list)
 
 my_list.append('e')
-print('After append: ', my_list)
+# print('After append: ', my_list)
+
+d = {'key1': 'value1', 'key2': 'abcde'}
+# print('this is entire dictionary for d: ', d)
+# print('this is the value at key1: ', d['key1'])
+# print('this is the value at key2: ', d['key2'])
+
+tuple1 = ('a', 'b', 'c')
+tuple2 = (1, 2, 3, 4)
+print('tuple1: ', tuple1)
+print('tuple2: ', tuple2)
