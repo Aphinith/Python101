78e47f850719c44f24c96262921b9744c2eda036
Aralya Phinith <aphinith84@gmail.com>
2017-01-18 00:31:17 -0800

    create variables using range and run for loops using range

--- dataTypes/loops.py
+++ dataTypes/loops.py
@@ -5,6 +5,12 @@ item_list = [1, 2, 3, 4, 5]
 
 # example of while loop
 i = 1
-while i < 5:
-  print('This is the value of i: {}'.format(i))
-  i = i + 1
+# while i < 5:
+  # print('This is the value of i: {}'.format(i))
+  # i = i + 
+
+first_range = list(range(0,10))
+print(first_range)
+
+for x in range(0,20):
+  print('testing x in loop using range: ', x)
