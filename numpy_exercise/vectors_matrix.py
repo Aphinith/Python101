ff8eb6687d559a9153f31a0bbf6c70de39f710c9
Aralya Phinith <aphinith84@gmail.com>
2017-01-18 22:41:01 -0800

    create a 2d array with numpy

--- numpy_exercise/vectors_matrix.py
+++ numpy_exercise/vectors_matrix.py
@@ -4,5 +4,9 @@ print(np)
 
 my_list = [1, 2, 3, 4]
 
-print('this is before using np: ', my_list)
-print(np.array(my_list))
+# print('this is before using np: ', my_list)
+# print(np.array(my_list))
+
+my_2D_list = [[1,2,3], [4,5,6], [7,8,9]]
+
+print(np.array(my_2D_list))
