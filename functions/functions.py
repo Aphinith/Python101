f7aface87ca57dcdc554cd9ac91dfcc7c8a5328d
Aralya Phinith <aphinith84@gmail.com>
2017-01-18 22:14:37 -0800

    create first vector using numpy

--- functions/functions.py
+++ functions/functions.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def my_func(param1):
   print(param1)
 
@@ -8,9 +10,15 @@ names = ['Aralya', 'Rosa', 'Tubesock']
 
 # list(map(my_func, names))
 
-list(map(lambda name: print(name), names))
+# list(map(lambda name: print(name), names))
 
 numbers = [1, 2, 3, 4, 5, 6]
 
 result = list(filter(lambda num: num%2 == 0, numbers))
-print(result)
+# print(result)
+
+print(np)
+my_list = [1, 2, 3, 4]
+
+print('this is before using np: ', my_list)
+print(np.array(my_list))
---
+++ numpy_exercise/vectors_matrix.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+print(np)
+
+my_list = [1, 2, 3, 4]
+
+print('this is before using np: ', my_list)
+print(np.array(my_list))
