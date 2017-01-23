bee171a5a283dd34028d32d94a69ea3a1d65de54
Aralya Phinith <aphinith84@gmail.com>
2017-01-22 22:45:13 -0800

    create data frame

--- panda_exercises/data_frames.py
+++ panda_exercises/data_frames.py
@@ -1,5 +1,10 @@
 import numpy as np 
 import pandas as pd 
+from numpy.random import randn
 
-print(np)
-print(pd)
+np.random.seed(101)
+
+# DataFrame takes in three arguments, data, index, and columns
+df = pd.DataFrame(randn(5, 4), ['A', 'B', 'C', 'D', 'E'], ['W', 'X', 'Y', 'Z'])
+
+print(df)
